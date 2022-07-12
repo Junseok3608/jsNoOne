5e928491ed5ecfab59d16b22266d59f480e9ef6e
JsKim <75792069+Junseok3608@users.noreply.github.com>
2022-07-12 21:50:00 +0900

    FEAT: js practice

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 const alertBox = document.querySelector(".alert-box");
+const alertBoxP = document.querySelector(".alert-box p");
+
+function alertControl(event, event2) {
+  alertBoxP.innerHTML = event;
+  alertBox.style.display = event2;
+}
