6c1a9f06e4a7744ebf5b0df8a376a8d1661f6270
JsKim <75792069+Junseok3608@users.noreply.github.com>
2022-07-19 15:00:03 +0900

    FEAT= practice tabs

---
+++ js/datail.js
@@ -0,0 +1,10 @@
+const tabBtn = document.querySelectorAll(".tab-button");
+const tabContent = document.querySelectorAll(".tab-content");
+
+// 버튼 누르면 1. tabbtn orange 다 지우기, tabcontent show 다 지우기
+// 그리고 눌린 버튼번호의 orange 추가, 해당 같은 번호의 show 추가
+// 계속 추가되면? tabbtn[이 번호]이번호에 해당하게 값이 바뀌도록
+
+tabBtn.forEach(function (a, b, c, d) {
+  console.log(a, b, c);
+});
