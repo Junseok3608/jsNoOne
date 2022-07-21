a43d18bf972cf0e0e4177e4b50db9112f3410aff
JsKim <75792069+Junseok3608@users.noreply.github.com>
2022-07-21 21:02:46 +0900

    FEAT: ajax practice

--- js/list.js
+++ js/list.js
@@ -1,9 +1,50 @@
-products.forEach((data) => {
+products.forEach((data, i) => {
   const cardSectionLayout = `
-  <div class="col-sm-4">
-    <img src="https://via.placeholder.com/600" class="w-100" />
-    <h5>${data.title}</h5>
-    <p>${data.price}</p>
-  </div>`;
+    <div class="col-sm-4">
+      <img src="https://via.placeholder.com/600" class="w-100" />
+      <h5>${data.title}</h5>
+      <p>${data.price}</p>
+    </div>`;
   document.querySelector(".row").insertAdjacentHTML("beforeend", cardSectionLayout);
+});
+
+let btnClicks = 0;
+document.querySelector("#more-btn").addEventListener("click", function () {
+  btnClicks++;
+  if (btnClicks == 1) {
+    fetch("https://codingapple1.github.io/js/more1.json")
+      .then((res) => res.json())
+      .then(function (data) {
+        for (i = 0; i < data.length; i++) {
+          const cardSectionLayout = `
+          <div class="col-sm-4">
+            <img src="https://via.placeholder.com/600" class="w-100" />
+            <h5>${data[i].title}</h5>
+            <p>${data[i].price}</p>
+          </div>`;
+          document.querySelector(".row").insertAdjacentHTML("beforeend", cardSectionLayout);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } else if (btnClicks == 2) {
+    fetch("https://codingapple1.github.io/js/more2.json")
+      .then((res) => res.json())
+      .then(function (data) {
+        for (i = 0; i < data.length; i++) {
+          const cardSectionLayout = `
+          <div class="col-sm-4">
+            <img src="https://via.placeholder.com/600" class="w-100" />
+            <h5>${data[i].title}</h5>
+            <p>${data[i].price}</p>
+          </div>`;
+          document.querySelector(".row").insertAdjacentHTML("beforeend", cardSectionLayout);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    document.querySelector("#more-btn").style.visibility = "hidden";
+  }
 });
